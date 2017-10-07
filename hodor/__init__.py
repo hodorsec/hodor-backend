89027f62b828cc2b378d08846f4913e7ebb2e76c
Sachin S. Kamath <sskamath96@gmail.com>
2017-10-07 16:51:37 +0530

    Create structure for API and secret key warning
    Signed-off-by: Sachin S. Kamath <sskamath96@gmail.com>

--- hodor/__init__.py
+++ hodor/__init__.py
@@ -2,8 +2,18 @@
 __version__ = '0.1'
 from flask import Flask
 from flask_debugtoolbar import DebugToolbarExtension
-app = Flask('hodor')
-app.config['SECRET_KEY'] = 'random'
-app.debug = True
-toolbar = DebugToolbarExtension(app)
 from hodor.controllers import *
+
+ENV = 'development'
+
+app = Flask('hodor')
+api = Api(app)
+app.config['SECRET_KEY'] = 'CHANGE_ME_IN_PRODUCTION'
+
+if ENV == 'development':
+    app.debug = True
+    toolbar = DebugToolbarExtension(app)
+else:
+    app.debug = False
+    if app.config['SECRET_KEY'] == 'CHANGE_ME_IN_PRODUCTION':
+        print("[!] Please change your secret key before running in production");
