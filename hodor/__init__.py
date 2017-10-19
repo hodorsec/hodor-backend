732c9cb4b4a31ba72314e0b4f76064ab850bd5f8
Sachin S. Kamath <sskamath96@gmail.com>
2017-10-19 15:57:55 +0530

    Restructure code and add user model
    Signed-off-by: Sachin S. Kamath <sskamath96@gmail.com>

--- hodor/__init__.py
+++ hodor/__init__.py
@@ -1,19 +1,35 @@
-# -*- coding: utf-8 -*-
-__version__ = '0.1'
-from flask import Flask
-from flask_debugtoolbar import DebugToolbarExtension
+# app/__init__.py
+import time
+from flask_api import FlaskAPI
+from flask_sqlalchemy import SQLAlchemy
+# local import
+from instance.config import app_config
 
-ENV = 'development'
+# initialize sql-alchemy
+db = SQLAlchemy()
 
-app = Flask('hodor')
-app.config['SECRET_KEY'] = 'CHANGE_ME_IN_PRODUCTION'
+from hodor.models import *
+
+
+def create_app(config_name):
+    app = FlaskAPI(__name__, instance_relative_config=True)
+    app.config.from_object(app_config[config_name])
+    app.config.from_pyfile('config.py')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.init_app(app)
+    if config_name != 'production':
+        for x in range(0,5):
+            print "*** YOU ARE NOT RUNNING THE APP IN PRODUCTION MODE!! *** CTRL + C TO ABORT. APP WILL RUN IN {} SECONDS".format(5-x)
+            time.sleep(1)
+
+    if config_name == 'production':
+        app.config['DEFAULT_RENDERERS'] = [
+            'flask.ext.api.renderers.JSONRenderer',
+        ]
+
+    return app
+
+
+app = create_app('testing')
 
 from hodor.controllers import *
-
-if ENV == 'development':
-    app.debug = True
-    toolbar = DebugToolbarExtension(app)
-else:
-    app.debug = False
-    if app.config['SECRET_KEY'] == 'CHANGE_ME_IN_PRODUCTION':
-        print("[!] Please change your secret key before running in production");
--- runserver.py
+++ runserver.py
@@ -1,8 +1,6 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-import os
 from hodor import app
 
+config_name = 'development'
+
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 8080))
-    app.run('0.0.0.0', port=port)
+    app.run()
