11da03aa703da6952a28ee0d3e9ca5f23640962a
Vasanth Viswanath S <cb.en.u4cse14254@cb.students.amrita.edu>
2017-10-25 22:46:57 +0530

    Add challenge model

---
+++ hodor/models/challenges.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from hodor import db
+from sqlalchemy import inspect
+
+
+class Challenges(db.Model):
+    __tablename__= 'challenges'
+
+    #Data variables for each Challenges
+    chalid=db.Column(db.String(32), primary_key=True,unique=True,nullable=False)
+    name = db.Column(db.String(32), nullable=False)
+    points = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.String(2048), nullable=False)
+
+    @staticmethod
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def get_all():
+        return Challenges.query.all()
